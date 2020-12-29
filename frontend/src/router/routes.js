554b7b1164fef9503e49c0d7169a4352770905b6
Natalie Pitts <68853754+nataliePitts@users.noreply.github.com>
2020-12-29 22:19:19 +0200

    adding  forgot my password + main page

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -17,6 +17,19 @@ const routes = [{
         component: () =>
             import ('pages/Categories.vue')
     },
+                    {
+        path: '/forgotPassword',
+        component: () =>
+            import ('components/forgotPassword.vue')
+    },
+    {
+        path: '/main',
+        component: () => import('layouts/MainFinal.vue'),
+        children: [
+          { path: '/main', component: () => import('pages/Main.vue') },
+          { path: '/myRecipes', component: () => import('pages/myRecipes.vue') }
+        ]
+    },
     // Always leave this as last one,
     // but you can also remove it
     {
