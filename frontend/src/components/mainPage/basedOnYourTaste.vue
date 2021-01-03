bc96e4decea2f8db216a12ecf09210fb7c0a6970
Natalie Pitts <68853754+nataliePitts@users.noreply.github.com>
2021-01-03 18:46:34 +0200

    Update basedOnYourTaste.vue
    Upt style

--- frontend/src/components/mainPage/basedOnYourTaste.vue
+++ frontend/src/components/mainPage/basedOnYourTaste.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
   <div class="q-pa-md">
     <q-carousel
       v-model="yourTaste"
@@ -13,113 +13,85 @@
       class="bg-black shadow-4 rounded-borders"
     >
       <q-carousel-slide :name="1" class="column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-          <q-img class="rounded-borders col-2 full-height" src="~assets/RomanoCheeseCrisp.png">
+      <div class="row fit justify q-col-gutter-md-start items-center q-gutter-md q-col-gutter-md no-wrap">
+        <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/cheeseCake.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
-            Romano Cheese Crisp
+           Cheese cake
           </div>
           <q-btn flat round color="red" icon="favorite_border" size="sm" />
           </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/CapreseonaStick.png">
+            </q-avatar>
+        </q-btn>
+        <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/HalloumiCheeseFingers.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
-            Caprese on a Stick
-          </div>
-          <q-btn flat round color="red" icon="favorite_border" size="sm" />
-          </q-img>
-           <q-img class="rounded-borders col-2 full-height" src="~assets/PineConeCheeseBall.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Pine Cone Cheese Ball
-          </div>
-          <q-btn flat round color="red" icon="favorite_border" size="sm" />
-          </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/BestEverPartyAppetizer.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Best Ever Party Appetizer
-          </div>
-          <q-btn flat round color="red" icon="favorite_border" size="sm" />
-          </q-img>
-           <q-img class="rounded-borders col-2 full-height" src="~assets/HalloumiCheeseFingers.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Halloumi Cheese Fingers
-          </div>
-          <q-btn flat round color="red" icon="favorite_border" size="sm" />
-          </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/curryCheeseBall.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Curry Cheese Ball
-          </div>
-          <q-btn flat round color="red" icon="favorite_border" size="sm" />
-          </q-img>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide :name="2" class="column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-          <q-img class="rounded-borders col-2 full-height" src="~assets/cheeseCake.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Cheese Cake
-          </div>
-          <q-btn flat round color="red" icon="favorite_border" size="sm" />
-          </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/HermanReunionCheeseBall.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Herman Reunion Cheese Ball
+           Halloumi Cheese Fingers
           </div>
           </q-img>
-           <q-img class="rounded-borders col-2 full-height" src="~assets/perfectBreakfast.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Perfect Breakfast
-          </div>
-          </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/AvocadoBreakfastSandwich.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Avocado Breakfast Sandwich
-          </div>
-          </q-img>
-           <q-img class="rounded-borders col-2 full-height" src="~assets/basicMashedPotatoes.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Top Mashed Potatoes
-          </div>
-          </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/cheeseCake.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Cheese Cake
-          </div>
-          </q-img>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide :name="3" class="column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-         <q-img class="rounded-borders col-2 full-height" src="~assets/PerfectCrabStuffedMushrooms.png">
-           <div class="absolute-full text-subtitle2 flex flex-center">
-            Perfect Crab-Stuffed Mushrooms
-          </div>
-          </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/PineConeCheeseBall.png">
+            </q-avatar>
+        </q-btn>
+        <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/PineConeCheeseBall.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
            Pine Cone Cheese Ball
           </div>
           </q-img>
-           <q-img class="rounded-borders col-2 full-height" src="~assets/RomanoCheeseCrisp.png">
+            </q-avatar>
+        </q-btn>
+            <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/RomanoCheeseCrisp.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
-            Romano Cheese Crisp
+           Romano Cheese Crisp
           </div>
           </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/CapreseonaStick.png">
+            </q-avatar>
+        </q-btn>
+           </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="2" class="column no-wrap">
+        <div class="row fit justify-start items-center q-gutter-md q-col-gutter-md no-wrap">
+               <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/CurryChicken.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
-            Caprese on a Stick
+           Five-Ingredient Red Curry Chicken
           </div>
           </q-img>
-           <q-img class="rounded-borders col-2 full-height" src="~assets/cheeseCake.png">
+            </q-avatar>
+        </q-btn>
+        <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/CurryChicken.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
-            Caption
+           Five-Ingredient Red Curry Chicken
           </div>
           </q-img>
-          <q-img class="rounded-borders col-2 full-height" src="~assets/cheeseCake.png">
+            </q-avatar>
+        </q-btn>
+        <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/CurryChicken.png">
            <div class="absolute-full text-subtitle2 flex flex-center">
-            Caption
+           Five-Ingredient Red Curry Chicken
           </div>
           </q-img>
-        </div>
+            </q-avatar>
+        </q-btn>
+            <q-btn class="items-center" to="/main" flat>
+            <q-avatar rounded size="170px" font-size="40px">
+          <q-img src="~assets/CurryChicken.png">
+           <div class="absolute-full text-subtitle2 flex flex-center">
+           Five-Ingredient Red Curry Chicken
+          </div>
+          </q-img>
+            </q-avatar>
+        </q-btn>
+           </div>
       </q-carousel-slide>
     </q-carousel>
   </div>
