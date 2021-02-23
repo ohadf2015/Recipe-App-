66dd916d54425ea8273e419d8211e8ae0f56087e
edenekd <70546180+edenekd@users.noreply.github.com>
2021-02-23 13:00:11 +0200

    margin

--- frontend/src/components/mainPage/recipeComponent.vue
+++ frontend/src/components/mainPage/recipeComponent.vue
@@ -6,7 +6,7 @@
            <div class="absolute-full text-subtitle1 flex flex-center">
           <div class="absolute-top flex flex-center">
                <div class="absolute-top flex flex-center">
-               <q-btn v-model="user_like" :value="recipeCard.name" flat round color="red" icon="favorite_border" size="sm" />
+              
                </div>
                <br>
                {{ recipeCard.name }}
@@ -15,6 +15,7 @@
           </q-img>
         </q-avatar>  
         </q-btn>
+         <q-btn @click="toggleLike(recipeCard.name)" flat round color="red" icon="favorite_border" size="sm" />
         <br>
         </div>
        
@@ -29,7 +30,6 @@ export default {
   data () {
     return {
       liked:false,
-      likesCount:null,
       user_likes:[]
     }
   },
@@ -41,14 +41,19 @@ export default {
       console.log(this.recipeCard)
     },
     beforeUpdate() {
-      console.log(this.user_likes)
+      console.log("user_like"+this.user_likes)
     },
   methods: {
-        // toggleLike: function() {
-        //     this.liked = ! this.liked;
-
-        //     this.liked ? this.likesCount++ : this.likesCount--;
-        // }
+        toggleLike(name) {
+          console.log("recpie name "+ name);
+            this.liked = ! this.liked;
+            if(this.liked){
+              this.user_likes.push(name);
+            }
+            else
+            this.user_likes.splice(0,1)
+          console.log(this.user_likes)
+        }
     
   } 
 }   
